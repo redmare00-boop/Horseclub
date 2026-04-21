1f4a061a3d0790e058a1caf7d93e5d748c3836ff
Redmare <redmare00@gmail.com>
2026-04-21 11:46:31 +0300

    Ленивое создание pool

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,28 +1,21 @@
 const { Pool } = require('pg')
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'есть' : 'нет')
-console.log('PG_URL:', process.env.PG_URL ? 'есть' : 'нет')
-console.log('NODE_ENV:', process.env.NODE_ENV)
+function getPool() {
+  const connStr = process.env.DATABASE_URL || process.env.PG_URL
+  return new Pool(
+    connStr
+      ? {
+          connectionString: connStr,
+          ssl: { rejectUnauthorized: false }
+        }
+      : {
+          user: 'postgres',
+          host: 'localhost',
+          database: 'horseclub',
+          password: 'Rapsodia01',
+          port: 5432,
+        }
+  )
+}
 
-const connStr = process.env.DATABASE_URL || process.env.PG_URL
-
-const pool = new Pool(
-  connStr
-    ? {
-        connectionString: connStr,
-        ssl: { rejectUnauthorized: false }
-      }
-    : {
-        user: 'postgres',
-        host: 'localhost',
-        database: 'horseclub',
-        password: 'Rapsodia01',
-        port: 5432,
-      }
-)
-
-pool.connect()
-  .then(() => console.log('База данных подключена'))
-  .catch(err => console.error('Ошибка подключения к БД:', err.message))
-
-module.exports = pool
+module.exports = { query: (...args) => getPool().query(...args) }
