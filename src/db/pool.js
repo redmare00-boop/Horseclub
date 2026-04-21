0e4689525408b58824dc0cf90f52e40460a538e0
Redmare <redmare00@gmail.com>
2026-04-21 10:14:47 +0300

    Настройка для деплоя на Railway

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,12 +1,17 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'horseclub',
-  password: 'Rapsodia01',
-  port: 5432,
-})
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    })
+  : new Pool({
+      user: 'postgres',
+      host: 'localhost',
+      database: 'horseclub',
+      password: 'Rapsodia01',
+      port: 5432,
+    })
 
 pool.connect()
   .then(() => console.log('База данных подключена'))
