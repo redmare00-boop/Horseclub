4d917faab95d9ae021908fc4c2ce3be29248d5d5
Redmare <redmare00@gmail.com>
2026-04-21 11:14:31 +0300

    Упрощение подключения к БД

--- src/db/pool.js
+++ src/db/pool.js
@@ -2,18 +2,21 @@ const { Pool } = require('pg')
 
 console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'есть' : 'нет')
 
-const pool = process.env.DATABASE_URL
-  ? new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
-    })
-  : new Pool({
-      user: 'postgres',
-      host: 'localhost',
-      database: 'horseclub',
-      password: 'Rapsodia01',
-      port: 5432,
-    })
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'horseclub',
+        password: 'Rapsodia01',
+        port: 5432,
+      }
+)
+
 pool.connect()
   .then(() => console.log('База данных подключена'))
   .catch(err => console.error('Ошибка подключения к БД:', err.message))
