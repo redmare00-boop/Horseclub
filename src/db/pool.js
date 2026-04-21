a6f18442fa2f89dd102d1896406cdea196dcea7c
Redmare <redmare00@gmail.com>
2026-04-21 11:02:13 +0300

    Исправление экспорта pool

--- src/db/pool.js
+++ src/db/pool.js
@@ -14,3 +14,8 @@ const pool = process.env.DATABASE_URL
       password: 'Rapsodia01',
       port: 5432,
     })
+pool.connect()
+  .then(() => console.log('База данных подключена'))
+  .catch(err => console.error('Ошибка подключения к БД:', err.message))
+
+module.exports = pool
