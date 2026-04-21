4a287e5ff8e8dbf7d377cba79ec40c37f6a61b18
Redmare <redmare00@gmail.com>
2026-04-21 21:40:18 +0300

    Increase json limit for photo upload

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '5mb' }))
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/api/bookings', bookingsRouter)
